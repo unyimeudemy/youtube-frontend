7edccbb55ac8e6eb81d24ed0d91ce64e445792e7
unyimeudoh <unyimeudemy20@gmail.com>
2023-08-01 11:54:54 +0100

    theme issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
